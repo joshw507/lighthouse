f6b0b5553dae05432e968ec6663ceca52d677074
joshw507 <joshua.wallace@unilever.com>
2020-10-27 14:56:58 +0000

    update

---
+++ gridsome/lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    collect: {
+      // Just use local host lhci figures out the port for the ci build
+      url: ["http://localhost:8080/"],
+      staticDistDir: "./gridsome/dist/",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 1 }],
+      },
+    },
+  },
+};
