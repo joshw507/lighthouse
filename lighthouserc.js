f77437c3e4eab8c11f92e6f39c359e4a833a3988
joshw507 <joshua.wallace@unilever.com>
2020-10-27 11:36:13 +0000

    update

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,23 +1,17 @@
 module.exports = {
-    ci: {
-        collect: {
-            // Just use local host lhci figures out the port for the ci build
-            url: [
-              "http://localhost:8080/",
-              
-            ],
-            staticDistDir: './gridsome/dist/',
-          },
-         upload: {
-            target: 'temporary-public-storage',
+  ci: {
+    collect: {
+      // Just use local host lhci figures out the port for the ci build
+      url: ["http://localhost:8080/"],
+      staticDistDir: "./gridsome/dist/",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 1 }],
       },
-      assert: {
-        assertions: {
-          'categories:performance': ['warn', {minScore: 1}],
     },
   },
-},
 };
-  
-
-  
