95af7953f86079522170cee0c558b6749fa499c1
joshw507 <joshua.wallace@unilever.com>
2020-10-27 15:24:21 +0000

    test

--- gridsome/lighthouserc.js
+++ gridsome/lighthouserc.js
@@ -2,15 +2,19 @@ module.exports = {
   ci: {
     collect: {
       // Just use local host lhci figures out the port for the ci build
-      url: ["http://localhost:8080/"],
-      staticDistDir: "./gridsome/dist/",
+      url: ["http://localhost/", "http://localhost/"],
+      staticDistDir: "./site/dist/",
     },
     upload: {
       target: "temporary-public-storage",
     },
     assert: {
       assertions: {
-        "categories:performance": ["warn", { minScore: 1 }],
+        "categories:performance": ["warn", { minScore: 0.8 }], // warning for now due to Akamai..
+        "categories:accessibility": ["error", { minScore: 1 }],
+        "categories:seo": ["error", { minScore: 0.92 }], // 0.92 due to robots file.
+        "categories:bestpractices": ["error", { minScore: 1 }],
+        "first-contentful-paint": ["warn", { maxNumericValue: 2000 }], // warning for now due to Akamai
       },
     },
   },
