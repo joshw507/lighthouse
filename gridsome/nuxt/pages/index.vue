e494dc04cf3e40e0aac2baac2a84cd1df975c330
joshw507 <joshua.wallace@unilever.com>
2020-10-30 11:50:30 +0000

    testing lighthouse

--- gridsome/nuxt/pages/index.vue
+++ gridsome/nuxt/pages/index.vue
@@ -20,6 +20,14 @@
         >
           GitHub
         </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub2
+        </a>
       </div>
     </div>
   </div>
