d498e7aa1edc063d9c05fecaca8a5bb40e5a02fc
joshw507 <joshua.wallace@unilever.com>
2020-10-30 13:02:29 +0000

    test1234

--- gridsome/nuxt/lighthouserc.js
+++ gridsome/nuxt/lighthouserc.js
@@ -2,20 +2,20 @@ module.exports = {
   ci: {
     collect: {
       // Just use local host lhci figures out the port for the ci build
-      url: ["http://localhost:8080/"],
-      staticDistDir: "./gridsome/dist/",
+      url: ['http://localhost:8080/'],
+      staticDistDir: './gridsome/nuxt/dist/',
     },
     upload: {
-      target: "temporary-public-storage",
+      target: 'temporary-public-storage',
     },
     assert: {
       assertions: {
-        "categories:performance": ["warn", { minScore: 0.8 }], // warning for now due to Akamai..
-        "categories:accessibility": ["error", { minScore: 1 }],
-        "categories:seo": ["error", { minScore: 0.92 }], // 0.92 due to robots file.
-        "categories:bestpractices": ["error", { minScore: 1 }],
-        "first-contentful-paint": ["warn", { maxNumericValue: 2000 }], // warning for now due to Akamai
+        'categories:performance': ['warn', { minScore: 0.8 }], // warning for now due to Akamai..
+        'categories:accessibility': ['error', { minScore: 1 }],
+        'categories:seo': ['error', { minScore: 0.92 }], // 0.92 due to robots file.
+        'categories:bestpractices': ['error', { minScore: 1 }],
+        'first-contentful-paint': ['warn', { maxNumericValue: 2000 }], // warning for now due to Akamai
       },
     },
   },
-};
+}
