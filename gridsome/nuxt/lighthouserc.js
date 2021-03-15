43a2247c3a74f42e2d5a93d3c176fd650df7ae65
joshw507 <joshua.wallace@unilever.com>
2021-03-15 09:40:14 +0000

    changing urls

--- gridsome/nuxt/lighthouserc.js
+++ gridsome/nuxt/lighthouserc.js
@@ -3,106 +3,16 @@ module.exports = {
     collect: {
       // Just use local host lhci figures out the port for the ci build
       url: [
-        'htpps://www.unilever.com/',
-
-        'htpps://www.hul.co.in/',
-
-        'htpps://www.unilever.pk/',
-
-        'htpps://www.unilever.com.lk/',
-
-        'htpps://www.unileverusa.com/',
-
-        'htpps://www.unilever.ca/',
-
-        'htpps://www.unilever.com.tr/',
-
-        'htpps://www.unilevermaghreb.com/',
-
-        'htpps://www.unileverme.com/',
-
-        'htpps://www.unilever.ru/',
-
-        'htpps://www.unilever.co.jp/',
-
-        'htpps://www.unilever.com.cn/',
-
-        'htpps://www.unilever.co.kr/',
-
-        'htpps://www.unilever.com.tw/',
-
-        'htpps://www.unilever.com.bd/',
-
-        'htpps://www.unilever.com.br/',
-
-        'htpps://www.unilever.com.mx/',
-
-        'htpps://www.unilever-middleamericas.com/',
-
-        'htpps://www.unilever.com.ar/',
-
-        'htpps://www.unilever.co.id/',
-
-        'htpps://www.unilever.com.my/',
-
-        'htpps://www.unilever.com.sg/',
-
-        'htpps://www.unilever.com.vn/',
-        'htpps://www.unilever.co.il/',
-
-        'htpps://www.unilever.co.uk/',
-
-        'htpps://www.unilever.co.il/',
-
-        'htpps://www.unilever.dk/',
-
-        'htpps://www.unilever.fi/',
-
-        'htpps://www.unilever.se/',
-
-        'htpps://www.unilever.com.ph/',
-
-        'htpps://www.unilever.co.th/',
-
-        'htpps://www.unilever.com.au/',
-
-        'htpps://www.unilever.fr/',
-
-        'htpps://www.unilever.be/',
-
-        'htpps://www.unilever.nl/',
-
-        'htpps://www.unilever.hu/',
-
-        'htpps://www.unilever.de/',
-
-        'htpps://www.unilever.es/',
-
-        'htpps://www.unilever.it/',
-
-        'htpps://www.unilever.cz/',
-
-        'htpps://www.unilever.sk/',
-
-        'htpps://www.unilever.gr/',
-
-        'htpps://www.unilever.pl/',
-
-        'htpps://www.unilever-fima.com/',
-
-        'htpps://www.unilever.ro/',
-
-        'htpps://www.unilever-ewa.com/',
-
-        'htpps://www.unilever.co.za/',
-
-        'htpps://www.unilever.com.hk',
-
-        'htpps://www.unilever.tt',
-
-        'htpps://www.unilever.ua/',
-
-        'htpps://www.unilever.ee/',
+        'https://www.unilever.com/',
+        'https://www.unilever.co.uk/',
+        'https://www.unilever.it/',
+        'https://www.hul.co.in/',
+        'https://www.unilever.pk/',
+        'https://www.unilever.com.lk/',
+        'https://www.unileverusa.com/',
+        'https://www.unilever.ca/',
+        'https://www.unilever.com.tr/',
+        'https://www.unilever.ru/',
       ],
     },
     upload: {
